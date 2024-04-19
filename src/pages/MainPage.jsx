7f17353cc29bbf56644c6f44844be2579da7fe11
dldnlee <sungeun8877@gmail.com>
2024-04-19 12:19:40 +0900

    [Feat] - MainPage markup 바꿈

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -6,7 +6,7 @@ import scorpion from 'src/assets/scorpion.png'
 import goat from 'src/assets/goat.png'
 import { useEffect, useState } from "react"
 import {motion} from 'framer-motion'
-import { Outlet, useNavigate } from "react-router-dom"
+import { Outlet, useNavigate, Link } from "react-router-dom"
 import { Radar } from "react-chartjs-2"
 // eslint-disable-next-line no-unused-vars
 import { Chart as ChartJS} from 'chart.js/auto'
@@ -44,72 +44,72 @@ const data = {
   }]
 };
 
-function WorkPref() {
-  return (
-    <div className="py-5 px-10 bg-gray-50">
-      <div className="flex justify-between pb-4 items-center">
-        <button className="none w-[25px] h-[25px]"></button>
-        <h2 className="text-2xl font-semibold">나의 직무 성향</h2>
-        <button className="w-[25px] h-[25px] flex justify-center items-center">
-          <img src={plus_square} alt="" className="w-full"/>
-        </button>
-      </div>
-      <div className="w-full py-10 flex justify-center items-center border-t border-b border-gray-400">
-        <Radar data={data} />
-      </div>
-    </div>
-  )
-}
+// function WorkPref() {
+//   return (
+//     <div className="py-5 px-10 bg-gray-50">
+//       <div className="flex justify-between pb-4 items-center">
+//         <button className="none w-[25px] h-[25px]"></button>
+//         <h2 className="text-2xl font-semibold">나의 직무 성향</h2>
+//         <button className="w-[25px] h-[25px] flex justify-center items-center">
+//           <img src={plus_square} alt="" className="w-full"/>
+//         </button>
+//       </div>
+//       <div className="w-full py-10 flex justify-center items-center border-t border-b border-gray-400">
+//         <Radar data={data} />
+//       </div>
+//     </div>
+//   )
+// }
 
-function KeywordPref() {
-  return (
-    <div className="py-5 px-10 bg-white">
-      <div className="flex justify-between pb-4 items-center">
-        <button className="none w-[25px] h-[25px]"></button>
-        <h2 className="text-2xl font-semibold">관심 키워드</h2>
-        <button className="w-[25px] h-[25px] flex justify-center items-center">
-          <img src={plus_square} alt="" className="w-full"/>
-        </button>
-      </div>
-      <div className="w-full py-10 flex justify-center items-center border-t border-b border-gray-400">
-        <div className="w-[200px] h-[200px] bg-gray-300"></div>
-      </div>
-    </div>
-  )
-}
+// function KeywordPref() {
+//   return (
+//     <div className="py-5 px-10 bg-white">
+//       <div className="flex justify-between pb-4 items-center">
+//         <button className="none w-[25px] h-[25px]"></button>
+//         <h2 className="text-2xl font-semibold">관심 키워드</h2>
+//         <button className="w-[25px] h-[25px] flex justify-center items-center">
+//           <img src={plus_square} alt="" className="w-full"/>
+//         </button>
+//       </div>
+//       <div className="w-full py-10 flex justify-center items-center border-t border-b border-gray-400">
+//         <div className="w-[200px] h-[200px] bg-gray-300"></div>
+//       </div>
+//     </div>
+//   )
+// }
 
-function NPC({name, image, clickHandler}) {
-  return (
-    <motion.button 
-      whileHover={{scale:1.1}}
-      whileTap={{scale:0.9}}
-      type='button'
-      className="w-[120px] flex flex-col items-center gap-2 bg-white shadow-lg  p-2 pt-4 rounded-2xl"
-      onClick={clickHandler}>
-        <img src={image} alt="" className="w-[40px]"/>
-        <p>{name}</p>
-    </motion.button>
-  )
-}
+// function NPC({name, image, clickHandler}) {
+//   return (
+//     <motion.button 
+//       whileHover={{scale:1.1}}
+//       whileTap={{scale:0.9}}
+//       type='button'
+//       className="w-[120px] flex flex-col items-center gap-2 bg-white shadow-lg  p-2 pt-4 rounded-2xl"
+//       onClick={clickHandler}>
+//         <img src={image} alt="" className="w-[40px]"/>
+//         <p>{name}</p>
+//     </motion.button>
+//   )
+// }
 
-function NPCContainer() {
-  const navigate = useNavigate();
+// function NPCContainer() {
+//   const navigate = useNavigate();
 
-  function redirect(name) {
-    navigate(`survey/${name}`);
-  }
+//   function redirect(name) {
+//     navigate(`survey/${name}`);
+//   }
 
-  return(
-    <div className="w-full px-14">
-      <h3 className="w-full text-center py-4 border-b">다양한 친구들과 대화해보세요!</h3>
-      <div className="flex justify-center items-center w-full gap-4 py-5">
-        <NPC name='스펙이' image={lion} clickHandler={() => redirect('스펙이')} />
-        <NPC name='취향이' image={scorpion} clickHandler={() => redirect('취향이')} />
-        <NPC name='관심이' image={goat} clickHandler={() => redirect('관심이')} />
-      </div>
-    </div>
-  )
-}
+//   return(
+//     <div className="w-full px-14">
+//       <h3 className="w-full text-center py-4 border-b">다양한 친구들과 대화해보세요!</h3>
+//       <div className="flex justify-center items-center w-full gap-4 py-5">
+//         <NPC name='스펙이' image={lion} clickHandler={() => redirect('스펙이')} />
+//         <NPC name='취향이' image={scorpion} clickHandler={() => redirect('취향이')} />
+//         <NPC name='관심이' image={goat} clickHandler={() => redirect('관심이')} />
+//       </div>
+//     </div>
+//   )
+// }
 
 
 function HeaderTest({user}) {
@@ -163,21 +163,23 @@ function Categories() {
 
 function MainCard() {
   return (
-    <div className="w-full h-full text-black font-bold text-xl rounded-xl relative bg-[#e0f2ff] flex justify-center items-center">
-        <div className="flex absolute top-0 left-0 w-full justify-between p-5">
-          <h1>오늘의 질문</h1>
-          <button>
-            <img src={plus_square} alt="" />
-          </button>
-        </div>
-        <img src={horse3d} alt='Main Character' />
+    <Link to="survey/취향이" className="w-[300px] min-w-[300px] h-[450px] text-black font-bold text-xl rounded-2xl relative bg-[#e0f2ff] flex justify-center items-center">
+      <div className="flex absolute top-0 left-0 w-full justify-between p-5">
+        <h1>오늘의 질문</h1>
+        <button>
+          <img src={plus_square} alt="" />
+        </button>
       </div>
+      <img src={horse3d} alt='Main Character' />
+    </Link>
   )
 }
 
 function CardContainer() {
   return (
-    <div className="py-4 px-10 w-full h-[450px]">
+    <div className="py-4 px-10 flex w-full  overflow-auto gap-5">
+      <MainCard />
+      <MainCard />
       <MainCard />
     </div>
   )
