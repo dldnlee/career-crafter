54f05aff45bfeb97d1b23d5878c265ce7c0c3f34
dldnlee <sungeun8877@gmail.com>
2024-05-10 10:14:33 +0900

    [Feat] - Video plays inline feature added

--- src/components/CheckAnimation.jsx
+++ src/components/CheckAnimation.jsx
@@ -4,7 +4,7 @@
 export function CheckAnimation() {
   return (
     <>
-      <video width="150" height="150" preload="none" autoPlay="autoplay" loop="true" muted="muted">
+      <video width="150" height="150" preload="none" autoPlay="autoplay" loop="true" muted="muted" playsInline>
         <source src="https://cdn-icons-mp4.flaticon.com/512/15401/15401461.mp4" type="video/mp4" />
       </video>
     </>
